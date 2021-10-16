832755c29e47e77fb719564dc36dfccb95bfb143
Muhammed Abdelaal <muhammedaabdelaal@gmail.com>
2021-10-16 18:43:05 +0200

    U

--- iteru/iteru.py
+++ iteru/iteru.py
@@ -6,7 +6,7 @@ class Map(ipyleaflet.Map):
     def __init__(self,**kwargs):
 
         if 'center' not in kwargs:
-            kwargs['center'] = [27,31]
+            kwargs['center'] = [31,31]
 
         if 'zoom' not in kwargs:
             kwargs['zoom'] = 5
