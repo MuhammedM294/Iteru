3223a192f496096fc91110105a6777c01ba0f592
Muhammed Abdelaal <muhammedaabdelaal@gmail.com>
2021-10-17 18:39:43 +0200

    Update

--- iteru/iteru.py
+++ iteru/iteru.py
@@ -14,16 +14,25 @@ class Map(ipyleaflet.Map):
         if 'scroll_wheel_zoom' not in kwargs:
             kwargs['scroll_wheel_zoom'] = True
 
-
         super().__init__(**kwargs)
 
+        if 'height' not in kwargs:
+            self.layout.height = '550px'
+        else:
+            self.layout.height = kwargs['height']
 
         self.add_control(ipyleaflet.ScaleControl(position = 'bottomleft'))
         self.add_control(ipyleaflet.ZoomControl(position = 'topright'))
         self.add_control(ipyleaflet.FullScreenControl(position = 'topright'))
         self.add_control(ipyleaflet.LayersControl(position = 'topleft'))
+        self.add_control(ipyleaflet.DrawControl(position = 'topright'))
 
-        
+class Basemap(ipyleaflet.basemaps):
+
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        pass
+
 
 
     
