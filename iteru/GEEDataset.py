abd83bafaf56cd77f61288050c892b9beabd8c03
Muhammed Abdelaal <muhammedaabdelaal@gmail.com>
2021-10-27 20:33:05 +0200

    UPDATE

--- iteru/GEEDataset.py
+++ iteru/GEEDataset.py
@@ -2,7 +2,7 @@ import ee
 from ipywidgets import *
 
 
-terrain = { 
+terrain_dataset = { 
 
     
     "AHN Netherlands 0.5m DEM, Interpolated":
@@ -13,10 +13,6 @@ terrain = {
 
     "AHN Netherlands 0.5m DEM, Raw Samples":
                                                      ee.Image("AHN/AHN2_05M_RUW"),
-
-    "Australian 5M DEM": 
-
-                                                     ee.ImageCollection("AU/GA/AUSTRALIA_5M_DEM"),
 
     "DEM-H: Australian SRTM Hydrologically Enforced Digital Elevation Model":
 
@@ -93,6 +89,67 @@ terrain = {
     "US NED Topographic Diversity ":
 
                                                    ee.Image("CSP/ERGo/1_0/US/topoDiversity"),
+    
+    "MERIT DEM: Multi-Error-Removed Improved-Terrain DEM":
+
+                                                    ee.Image("MERIT/DEM/v1_0_3"),
+    
+    "MERIT Hydro: Global Hydrography Datasets":
+
+                                                    ee.Image("MERIT/Hydro/v1_0_1"),
+
+    "MERIT Hydro: Supplementary Visualization Layers":
+
+                                                    ee.Image("MERIT/Hydro_reduced/v1_0_1"),
+
+    "AG100: ASTER Global Emissivity Dataset 100-meter V003":
+
+                                                     ee.Image("NASA/ASTER_GED/AG100_003"),
+
+    "NASADEM: NASA NASADEM Digital Elevation 30m":
+
+                                                     ee.Image("NASA/NASADEM_HGT/001"),
+
+    "ETOPO1: Global 1 Arc-Minute Elevation":
+
+                                                     ee.Image("NOAA/NGDC/ETOPO1"),
+
+    
+    "USGS 3DEP National Map Seamless 1/3 Arc-Second (10m)":
+
+                                                     ee.Image("USGS/3DEP/10m"),
+
+    "GMTED2010: Global Multi-resolution Terrain Elevation Data 2010":
+
+                                                      ee.Image("USGS/GMTED2010"),
+
+    "GTOPO30: Global 30 Arc-Second Elevation":
+
+                                                      ee.Image("USGS/GTOPO30"),
+
+    "NASA SRTM Digital Elevation 30m":
+
+                                                      ee.Image("USGS/SRTMGL1_003"),
+    
+    "WWF HydroSHEDS Hydrologically Conditioned DEM, 3 Arc-Seconds":
+
+                                                      ee.Image("WWF/HydroSHEDS/03CONDEM"),
+
+    "WWF HydroSHEDS Void-Filled DEM, 3 Arc-Seconds":
+
+                                                     ee.Image("WWF/HydroSHEDS/03VFDEM"),
+
+    "WWF HydroSHEDS Hydrologically Conditioned DEM, 15 Arc-Seconds":
+
+                                                     ee.Image("WWF/HydroSHEDS/15CONDEM"),
+
+    "WWF HydroSHEDS Hydrologically Conditioned DEM, 30 Arc-Seconds":
+
+                                                      ee.Image("WWF/HydroSHEDS/30CONDEM"),
+
+    "Australian 5M DEM": 
+
+                                                     ee.ImageCollection("AU/GA/AUSTRALIA_5M_DEM"),                                               
 
     "HYCOM: Hybrid Coordinate Ocean Model, Sea Surface Elevation":
 
@@ -100,7 +157,11 @@ terrain = {
 
     "ALOS DSM: Global 30m":
 
-                                                   ee.ImageCollection("JAXA/ALOS/AW3D30/V3_2")
+                                                   ee.ImageCollection("JAXA/ALOS/AW3D30/V3_2"),
+    "Canadian Digital Elevation Model":
+
+                                                     ee.ImageCollection("NRCan/CDEM")
+
 
 }
 
