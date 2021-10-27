6a0bb5483dddd1d3b94b0169a5f1427fbcfc30c6
Muhammed Abdelaal <muhammedaabdelaal@gmail.com>
2021-10-27 20:32:46 +0200

    UPDATE

--- iteru/iteru.py
+++ iteru/iteru.py
@@ -7,7 +7,8 @@ import os
 from ipywidgets import *
 
 
-from .Map_Widgets import *
+from .GEEDataset import *
+from .basemaps import *
 from .common import *
 from IPython.display import display
 
@@ -48,6 +49,7 @@ class Map(ipyleaflet.Map):
             self.layout.height = kwargs['height']
 
         super().__init__(**kwargs)
+
 
         self.add_control(ipyleaflet.SearchControl(
                          
@@ -120,15 +122,79 @@ class Map(ipyleaflet.Map):
 
         self.add_control(measure)  
 
+        
+        basemaps = Dropdown(
+                     options = basemaps_dataset,
+                     value = None,
+                     description = '',
+                     description_tooltip = 'Select Basemap',
+                     layout=Layout(width='200px'),
+                     style = {'description_width': 'initial'}
+               )
+
+        terrain = Dropdown(
+                     options = terrain_dataset,
+                     value = None,
+                     description = '',
+                     description_tooltip = 'GEE Terrain Dataset',
+                     layout=Layout(width='200px'),
+                     style = {'description_width': 'initial'}
+             )
+
+        basemaps_button = ToggleButton(
+                               value = False, 
+                               tooltip = 'Basemaps',
+                               icon = 'map',
+                               layout = Layout(width = '32px', height = '28px'),
+        )
+        terrain_dataset_button = ToggleButton(
+                               value = False, 
+                               tooltip = 'GEE Terrain Dataset',
+                               icon = 'server',
+                               layout = Layout(width = '32px', height = '28px'),
+        )
+        TOC = ToggleButton(
+                               value = False, 
+                               tooltip = 'Table of Contents',
+                               icon = 'archive',
+                               layout = Layout(width = '32px', height = '28px'),
+       )
+
+
+        basemap_tool = HBox([basemaps_button])
+        terrain_dataset_tool = HBox([terrain_dataset_button])
+        TOC_container = HBox([TOC])
+        TOC_out = Output(layout={'border': '1px solid blue'})
+
+        def basemap_tool_click(change):
+            if change.new:
+                basemap_tool.children = [basemaps_button,basemaps]
+            else:
+               basemap_tool.children = [basemaps_button]
+          
+        def terrain_tool_click(change):
+            if change.new:
+                terrain_dataset_tool.children = [terrain_dataset_button, terrain]
+            else:
+                terrain_dataset_tool.children = [terrain_dataset_button] 
+        
+        def TOC_container_click(change):
+            if change.new:
+                TOC_container.children = [TOC,TOC_out]
+            else:
+                TOC_container.children = [TOC]     
+
+        basemaps_button.observe(basemap_tool_click, names = 'value')
+        terrain_dataset_button.observe(terrain_tool_click, names = 'value')
+        TOC.observe(TOC_container_click, names = 'value') 
+
         basemap_tool_widget = ipyleaflet.WidgetControl(widget = basemap_tool, position = 'topleft')
         terrain_dataset_tool_widget = ipyleaflet.WidgetControl(widget = terrain_dataset_tool, position = 'topleft')
         TOC_widget = ipyleaflet.WidgetControl(widget = TOC_container, position = 'topleft') 
 
-        
         self.add_control(basemap_tool_widget)
         self.add_control(terrain_dataset_tool_widget)
         self.add_control(TOC_widget )
-
 
         def layer_widgets(change):
             if change.new:
@@ -194,10 +260,21 @@ class Map(ipyleaflet.Map):
             close_button.on_click(remove_layer) 
             
         basemaps.observe(layer_widgets, names = 'value')
+     
         terrain.observe(layer_widgets, names= 'value')
-    
-    
 
+    def zoom_to (self,ee_object, zoom = 8):
+        
+            try:
+                lat = ee_object.geometry().centroid().getInfo()['coordinates'][1]
+                long = ee_object.geometry().centroid().getInfo()['coordinates'][0]
+                self.center = (lat,long)
+                self.zoom = zoom
+            except:
+                self.center = (27,31)
+                self.zoom = 5
+                print('Error: can not get the centroid of the Object')    
+    
 
     def add_ee_layer(self, ee_object, vis_params=None, name = ''):
 
