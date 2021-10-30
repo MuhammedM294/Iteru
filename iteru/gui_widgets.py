66940aef4dd90382f8c7a18b3ccc02ea2a6293ec
Muhammed Abdelaal <muhammedaabdelaal@gmail.com>
2021-10-30 16:39:57 +0200

    UPDATE

---
+++ iteru/gui_widgets.py
@@ -0,0 +1,76 @@
+from .basemaps import basemaps_dataset
+from .GEEDataset import terrain_dataset
+from ipywidgets import *
+from ipyleaflet import WidgetControl
+
+
+basemaps = Dropdown(
+                     options = basemaps_dataset,
+                     value = None,
+                     description = '',
+                     description_tooltip = 'Select Basemap',
+                     layout=Layout(width='200px'),
+                     style = {'description_width': 'initial'}
+               )
+terrain = Dropdown(
+                     options = terrain_dataset,
+                     value = None,
+                     description = '',
+                     description_tooltip = 'GEE Terrain Dataset',
+                     layout=Layout(width='200px'),
+                     style = {'description_width': 'initial'}
+             )
+basemaps_button = ToggleButton(
+                               value = False, 
+                               tooltip = 'Basemaps',
+                               icon = 'map',
+                               layout = Layout(width = '32px', height = '28px'),
+        ) 
+terrain_dataset_button = ToggleButton(
+                               value = False, 
+                               tooltip = 'GEE Terrain Dataset',
+                               icon = 'server',
+                               layout = Layout(width = '32px', height = '28px'),
+        )   
+TOC = ToggleButton(
+                               value = False, 
+                               tooltip = 'Table of Contents',
+                               icon = 'archive',
+                               layout = Layout(width = '32px', height = '28px'),
+       )  
+
+basemap_tool = HBox([basemaps_button])
+terrain_dataset_tool = HBox([terrain_dataset_button])
+TOC_container = HBox([TOC])
+TOC_out = Output(layout={'border': '1px solid blue'})       
+
+def basemap_tool_click(change):
+    if change.new:
+        basemap_tool.children = [basemaps_button,basemaps]
+    else:
+        basemap_tool.children = [basemaps_button]
+          
+def terrain_tool_click(change):
+    if change.new:
+        terrain_dataset_tool.children = [terrain_dataset_button, terrain]
+    else:
+        terrain_dataset_tool.children = [terrain_dataset_button] 
+        
+def TOC_container_click(change):
+    if change.new:
+        TOC_container.children = [TOC,TOC_out]
+    else:
+        TOC_container.children = [TOC]     
+
+basemaps_button.observe(basemap_tool_click, names = 'value')
+terrain_dataset_button.observe(terrain_tool_click, names = 'value')
+TOC.observe(TOC_container_click, names = 'value') 
+
+basemap_tool_widget = WidgetControl(widget = basemap_tool, position = 'topleft')
+terrain_dataset_tool_widget = WidgetControl(widget = terrain_dataset_tool, position = 'topleft')
+TOC_widget = WidgetControl(widget = TOC_container, position = 'topleft')
+
+
+        
+                 
+            
--- iteru/iteru.py
+++ iteru/iteru.py
@@ -3,17 +3,14 @@
 
 import ipyleaflet 
 import ee
-import os
 from ipywidgets import *
 
-
-from .GEEDataset import *
-from .basemaps import *
+from .gui_widgets import *
 from .common import *
 from IPython.display import display
 
 
-def ee_tilelayer(ee_object, vis_params = None, name ='ee layer'):
+def ee_tilelayer(ee_object, vis_params = None, name =''):
         
         ee_object_id =ee_object.getMapId(vis_params)
 
@@ -121,115 +118,46 @@ class Map(ipyleaflet.Map):
         measure.primary_area_unit = ('Sq Kilometers')
 
         self.add_control(measure)  
-
-        
-        basemaps = Dropdown(
-                     options = basemaps_dataset,
-                     value = None,
-                     description = '',
-                     description_tooltip = 'Select Basemap',
-                     layout=Layout(width='200px'),
-                     style = {'description_width': 'initial'}
-               )
-
-        terrain = Dropdown(
-                     options = terrain_dataset,
-                     value = None,
-                     description = '',
-                     description_tooltip = 'GEE Terrain Dataset',
-                     layout=Layout(width='200px'),
-                     style = {'description_width': 'initial'}
-             )
-
-        basemaps_button = ToggleButton(
-                               value = False, 
-                               tooltip = 'Basemaps',
-                               icon = 'map',
-                               layout = Layout(width = '32px', height = '28px'),
-        )
-        terrain_dataset_button = ToggleButton(
-                               value = False, 
-                               tooltip = 'GEE Terrain Dataset',
-                               icon = 'server',
-                               layout = Layout(width = '32px', height = '28px'),
-        )
-        TOC = ToggleButton(
-                               value = False, 
-                               tooltip = 'Table of Contents',
-                               icon = 'archive',
-                               layout = Layout(width = '32px', height = '28px'),
-       )
-
-
-        basemap_tool = HBox([basemaps_button])
-        terrain_dataset_tool = HBox([terrain_dataset_button])
-        TOC_container = HBox([TOC])
-        TOC_out = Output(layout={'border': '1px solid blue'})
-
-        def basemap_tool_click(change):
-            if change.new:
-                basemap_tool.children = [basemaps_button,basemaps]
-            else:
-               basemap_tool.children = [basemaps_button]
-          
-        def terrain_tool_click(change):
-            if change.new:
-                terrain_dataset_tool.children = [terrain_dataset_button, terrain]
-            else:
-                terrain_dataset_tool.children = [terrain_dataset_button] 
-        
-        def TOC_container_click(change):
-            if change.new:
-                TOC_container.children = [TOC,TOC_out]
-            else:
-                TOC_container.children = [TOC]     
-
-        basemaps_button.observe(basemap_tool_click, names = 'value')
-        terrain_dataset_button.observe(terrain_tool_click, names = 'value')
-        TOC.observe(TOC_container_click, names = 'value') 
-
-        basemap_tool_widget = ipyleaflet.WidgetControl(widget = basemap_tool, position = 'topleft')
-        terrain_dataset_tool_widget = ipyleaflet.WidgetControl(widget = terrain_dataset_tool, position = 'topleft')
-        TOC_widget = ipyleaflet.WidgetControl(widget = TOC_container, position = 'topleft') 
-
         self.add_control(basemap_tool_widget)
         self.add_control(terrain_dataset_tool_widget)
         self.add_control(TOC_widget )
 
-        def layer_widgets(change):
-            if change.new:
-                if type(change.new) is ee.image.Image:
-                    layer = ee_tilelayer(change.new)
-                    name = change.new.getInfo()['id']
-                
-                elif type(change.new) is ipyleaflet.leaflet.TileLayer:
-                     layer = change.new
-                     name = change.new.name
-                
-            if layer in self.layers:
-                pass
-            else:  
-                self.add_layer(layer)
-            
-                close_button = Button(  
+    def add_layer_widgets(self, object, vis_params = None, name =None):
+        try:
+            if type(object) is ee.image.Image:
+                layer = ee_tilelayer(object,vis_params)
+                if name is None:
+                   name = object.getInfo()['id']
+        
+            elif type(object) is ipyleaflet.leaflet.TileLayer:
+                layer = object
+                if name is None:
+                   name = object.name
+        except:
+            print('input layer is not supported ')
+     
+        try:
+            self.add_layer(layer)
+
+            close_button = Button(  
                                 value = False, 
                                 tooltip = 'Remove This Layer',
                                 icon = 'window-close',
                                 layout = Layout(width = '32px', height = '28px'),   
                 )
 
-                layer_visibility = Checkbox(
+            layer_visibility = Checkbox(
                                value = True, 
                                indent=False,
                                layout = Layout(width = '15px')
                 )
                 
-                layer_name = Label(
+            layer_name = Label(
                                    value= name,
                                    layout = Layout(width = '150px', height = '28px'),
                                    tooltip = name
                )
-                opacity = FloatSlider(
+            opacity = FloatSlider(
                                       value = 1,
                                       min = 0,  
                                       max = 1,
@@ -240,28 +168,33 @@ class Map(ipyleaflet.Map):
                 layer.visible = show_hide 
                     
             def change_layer_opacity(Opacity):
-                layer.opacity = Opacity
+                 layer.opacity = Opacity
                         
             def remove_layer(b):
-                self.remove_layer(layer)
-                close_button.close()
-                opacity.close()
-                layer_name.close()
-                layer_visibility.close()
+                 self.remove_layer(layer)
+                 close_button.close()
+                 opacity.close()
+                 layer_name.close()
+                 layer_visibility.close()
                 
             opacity_slider = interactive(change_layer_opacity,Opacity = opacity)
                 
             visibility_checkbox = interactive(change_layer_visibility,show_hide = layer_visibility)
                 
             with TOC_out:
-                
                 display(HBox([layer_visibility,layer_name,opacity, close_button]))
                    
             close_button.on_click(remove_layer) 
+        
+        except Exception:
+             print(f'layer already on map:{layer}')
+
+    def layer_widgets(self, change):
+        if change.new:
+            self.add_layer_widgets(change)
             
-        basemaps.observe(layer_widgets, names = 'value')
-     
-        terrain.observe(layer_widgets, names= 'value')
+    basemaps.observe(layer_widgets, names = 'value')
+    terrain.observe(layer_widgets, names= 'value')
 
     def zoom_to (self,ee_object, zoom = 8):
         
