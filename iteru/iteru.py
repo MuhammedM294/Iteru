ea48b7c4c3791d9ba602185493408cf55c74473a
Muhammed Abdelaal <muhammedaabdelaal@gmail.com>
2022-03-17 13:29:06 +0100

    Update

--- iteru/iteru.py
+++ iteru/iteru.py
@@ -1,17 +1,20 @@
 """The main module for the interactive mapping based on Google Earth Enigne Python API and Ipyleaflet Package """
 import ee
-import matplotlib.pyplot as plt
 import datetime
 import geemap
 import ipyleaflet
+import matplotlib.pyplot as plt
+
 from ipywidgets import *
 from .gui_widgets import *
 from .common import *
 from IPython.display import display
 
+ee.Initialize()
+
 
 class Map(geemap.Map):
-    """ Inherting the Map class from Ipyleaflet with the all its methods and attributes
+    """ Inherting the Map class from geemap with the all its methods and attributes
 
     """
 
