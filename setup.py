d669c1240ffaa046e99812d2d4abadb62b5b845f
Muhammed Abdelaal <muhammedaabdelaal@gmail.com>
2022-05-18 07:23:35 +0200

    Bump version: 0.0.24 → 0.0.25

--- setup.py
+++ setup.py
@@ -54,6 +54,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/MuhammedM294/iteru',
-    version='0.0.24',
+    version='0.0.25',
     zip_safe=False,
 )
