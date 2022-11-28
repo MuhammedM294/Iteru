7f994b172c4b43dc686f64e086a6c57c033822dd
M294 <muhammedaabdelaal@gmail.com>
2022-11-28 21:30:24 +0200

    Bump version: 0.0.31 → 0.0.32

--- iteru/__init__.py
+++ iteru/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Muhammed Abdelaal"""
 __email__ = 'muhammedaabdelaal@gmail.com'
-__version__ = '0.0.31'
+__version__ = '0.0.32'
 
 
 from .map import *
--- setup.py
+++ setup.py
@@ -54,6 +54,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/MuhammedM294/iteru',
-    version='0.0.31',
+    version='0.0.32',
     zip_safe=False,
 )
