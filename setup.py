0b82d1e9354b80070cf8ae80aeb25905250d1b93
M294 <muhammedaabdelaal@gmail.com>
2022-11-29 01:04:45 +0200

    Bump version: 0.0.36 → 0.0.37

--- setup.py
+++ setup.py
@@ -54,6 +54,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/MuhammedM294/iteru',
-    version='0.0.36',
+    version='0.0.37',
     zip_safe=False,
 )
